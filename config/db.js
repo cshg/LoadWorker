35980d4c9f9fdf9602936e073d7fb06bbb641d7b
cshg <csh@christianhaug.com>
2016-07-07 17:18:19 -0700

    Add port to mysql connection

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ const connection = {
   client: 'mysql',
   connection: {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     database: process.env.APP_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
